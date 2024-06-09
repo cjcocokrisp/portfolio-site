0e2a0a27caecf6b49f2b3694e361ad96dcfd210c
Christopher <chriscoco1205@gmail.com>
2024-06-09 15:13:18 -0400

    Move JSON contents

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,15 +2,13 @@
 import { promises as fs } from 'fs';
 
 export async function loadWorkData() {
-    console.log(process.cwd() + '/data/work.json')
-    const workFile = await fs.readFile(process.cwd() + '/data/work.json', 'utf-8');
+    const workFile = await fs.readFile(process.cwd() + '/src/app/data/work.json', 'utf-8');
     const workData = JSON.parse(workFile);
     return workData;
 }
 
 export async function loadProjectData() {
-    console.log(process.cwd() + '/data/projects.json');
-    const projectFile = await fs.readFile(process.cwd() + '/data/projects.json', 'utf-8')
+    const projectFile = await fs.readFile(process.cwd() + '/src/app/data/projects.json', 'utf-8')
     const projectData = JSON.parse(projectFile);
     return projectData
 }
