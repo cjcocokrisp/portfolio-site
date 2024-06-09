a20d8467ac1d416c4f6c7fd5594dbb25f21eca5a
Christopher <chriscoco1205@gmail.com>
2024-06-09 15:29:53 -0400

    Another attempt to fix JSON bug

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,13 +2,13 @@
 import { promises as fs } from 'fs';
 
 export async function loadWorkData() {
-    const workFile = await fs.readFile(process.cwd() + '/src/app/data/work.json', 'utf-8');
+    const workFile = await fs.readFile('/data/work.json', 'utf8');
     const workData = JSON.parse(workFile);
     return workData;
 }
 
 export async function loadProjectData() {
-    const projectFile = await fs.readFile(process.cwd() + '/src/app/data/projects.json', 'utf-8')
+    const projectFile = await fs.readFile('/data/projects.json', 'utf8')
     const projectData = JSON.parse(projectFile);
     return projectData
 }
